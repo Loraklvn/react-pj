d4580d3e067cbd173578645e8835f9bce1edb699
Kelvin Lora <kelvinlora5@gmail.com>
2024-02-06 08:52:18 -0400

    :sparkles add a serverless function

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Counter from './pages/Counter';
 import Home from './pages/Home';
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    fetch(
+      'https://function-demoapp.azurewebsites.net/api/HttpTrigger1?name=Kelvin&code=_2DRQtmHN6bINEvAMt3a2dwP59qXQyhIXZEierbhC-S6AzFu6L4RiA=='
+    ).then(async (response) => {
+      const res = await response.json();
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div className="max-w-7xl mx-auto px-4">
       <Navbar />
